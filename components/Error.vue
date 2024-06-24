569f8420c48d2854d461afbc5b0dd69f1bf2d00f
Jake Goodman <jmgoodman27@gmail.com>
2024-06-24 12:58:05 -0400

    Build error component with ts props to handle api errors

---
+++ components/Error.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        error?: string
+    }>();
+</script>
+
+<template>
+    <div v-if="props.error" class="w-fit bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+        <p class="error">{{ props.error }}</p>
+    </div>
+</template>
